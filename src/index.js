359aed30d8094fc0f106c647767cf5abb4f60b05
scarlet <scarlet@debian.vortex>
2019-04-13 16:20:50 -0400

    updateing package/package lock

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './components/App.jsx';
 // Require Sass file so webpack can build it
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import style from './styles/style.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
